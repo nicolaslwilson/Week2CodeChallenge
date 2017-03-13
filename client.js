cc66ffcc04462019abc677e1f2f9b0d6696e7a5f
Nic Wilson <nicwilson@gmail.com>
2017-03-13 13:20:10 -0500

    Final solution

--- client.js
+++ client.js
@@ -1,3 +1,39 @@
+var clickTally = 0;
+
 $(document).ready(function() {
-  
+  addEventListeners();
 });
+
+
+function addEventListeners () {
+  $('#generate').on('click', generateDiv);
+  $('.container').on('click', '.swap', swapBG);
+  $('.container').on('click', '.delete', removeDiv);
+}
+
+function generateDiv () {
+  clickTally++;
+  $('.container').append('<div class="red"></div>');
+  var $el = $('.container').children().last();
+  $el.append('<p>' + clickTally + '</p>');
+  $el.append('<button class="swap">Swap</button>');
+  $el.append('<button class="delete">Delete</button>');
+}
+
+function swapBG () {
+  var $el = $(this).parent();
+  if ($el.hasClass('red')) {
+    $el.addClass('yellow');
+    $el.removeClass('red');
+  }
+  else if ($el.hasClass('yellow')) {
+    $el.addClass('red');
+    $el.removeClass('yellow');
+  }
+}
+
+function removeDiv () {
+  var $el = $(this).parent();
+  $el.remove();
+  console.log($(this).parent());
+}
